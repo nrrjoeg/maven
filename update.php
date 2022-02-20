17093d295c958acc518d5be457190e393081b584
Quincy Adams Wagstaff <reeperbahnmedia@gmail.com>
2022-02-20 16:39:59 -0500

    First fixes to update.php

--- update.php
+++ update.php
@@ -3,56 +3,78 @@
 require_once "config.php";
  
 // Define variables and initialize with empty values
-$name = $address = $salary = "";
-$name_err = $address_err = $salary_err = "";
+$firstname = $lastname = $couponcode = $email = $city = $state = "";
+$firstname_err = $lastname_err = $couponcode_err = $email_err = $city_err = $state_err = "";
  
 // Processing form data when form is submitted
-if(isset($_POST["id"]) && !empty($_POST["id"])){
+if(isset($_POST["ID"]) && !empty($_POST["ID"])){
     // Get hidden input value
-    $id = $_POST["id"];
+    $ID = $_POST["ID"];
     
-    // Validate name
-    $input_name = trim($_POST["name"]);
-    if(empty($input_name)){
-        $name_err = "Please enter a name.";
-    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $name_err = "Please enter a valid name.";
+    // Validate first name
+    $input_firstname = trim($_POST["FirstName"]);
+    if(empty($input_firstname)){
+        $firstname_err = "Please enter a first name.";
     } else{
-        $name = $input_name;
+        $firstname = $input_firstname;
+    }
+
+    // Validate last name
+    $input_lastname = trim($_POST["LastName"]);
+    if(empty($input_lastname)){
+        $lastname_err = "Please enter Maven last name.";     
+    } else{
+        $lastname = $input_lastname;
     }
     
-    // Validate address address
-    $input_address = trim($_POST["address"]);
-    if(empty($input_address)){
-        $address_err = "Please enter an address.";     
-    } else{
-        $address = $input_address;
-    }
-    
-    // Validate salary
-    $input_salary = trim($_POST["salary"]);
-    if(empty($input_salary)){
-        $salary_err = "Please enter the salary amount.";     
-    } elseif(!ctype_digit($input_salary)){
-        $salary_err = "Please enter a positive integer value.";
-    } else{
-        $salary = $input_salary;
-    }
+   // Validate Coupon Code
+   $input_couponcode = trim($_POST["CouponCode"]);
+   if(empty($input_couponcode)){
+       $couponcode_err = "Please enter a Coupon Code.";     
+   } else{
+       $couponcode = $input_couponcode;
+   }
+   
+   // Validate email
+   $input_email = trim($_POST["Email"]);
+   if(empty($input_email)){
+       $email_err = "Please enter Maven Email address.";     
+   } else{
+       $email = $input_email;
+   }
+   
+   // Validate city
+   $input_city = trim($_POST["City"]);
+   if(empty($input_city)){
+       $city_err = "Please enter Maven City.";     
+   } else{
+       $city = $input_city;
+   }
+
+   // Validate state
+   $input_state = trim($_POST["State"]);
+   if(empty($input_state)){
+       $state_err = "Please enter Maven State.";     
+   } else{
+       $state = $input_state;
+   }
     
     // Check input errors before inserting in database
     if(empty($name_err) && empty($address_err) && empty($salary_err)){
         // Prepare an update statement
-        $sql = "UPDATE employees SET name=?, address=?, salary=? WHERE id=?";
+        $sql = "UPDATE employees SET FirstName=?, LastName=?, CouponCode=?, Email=?, City=?, State=? WHERE ID=?";
          
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sssi", $param_name, $param_address, $param_salary, $param_id);
+            mysqli_stmt_bind_param($stmt, "ssssss", $param_firstname, $param_lastname, $param_couponcode, $param_email, $param_city, $param_state);
             
             // Set parameters
-            $param_name = $name;
-            $param_address = $address;
-            $param_salary = $salary;
-            $param_id = $id;
+            $param_firstname = $firstname;
+            $param_lastname = $lastname;
+            $param_couponcode = $couponcode;
+            $param_email = $email;
+            $param_city = $city;
+            $param_state = $state;
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
