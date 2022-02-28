b0051ff8c7a7aa474544d9f449685c3e0ee4a807
Rufus T. Firefly <naturesritejoeg@gmail.com>
2022-02-28 16:27:40 -0500

    Updates to list-maven-order.php but still doesn't work

--- list-maven-orders.php
+++ list-maven-orders.php
@@ -1,62 +1,6 @@
-<?php
-// Check existence of id parameter before processing further
-if(isset($_GET["ID"]) && !empty(trim($_GET["ID"]))){
-    // Include config file
-    require_once "config.php";
-    
-    // Prepare a select statement
-    $sql = "SELECT * FROM `Orders` WHERE ID = ?";
-    
-    if($stmt = mysqli_prepare($link, $sql)){
-        // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt, "i", $param_id);
-        
-        // Set parameters
-        $param_id = trim($_GET["ID"]);
-        
-        // Attempt to execute the prepared statement
-        if(mysqli_stmt_execute($stmt)){
-            $result = mysqli_stmt_get_result($stmt);
-    
-            if(mysqli_num_rows($result) == 1){
-                /* Fetch result row as an associative array. Since the result set
-                contains only one row, we don't need to use while loop */
-                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-                
-                // Retrieve individual field value
-                $custid = $row["CustID"];
-                $ordernumber = $row["OrderNumber"];
-                $couponcode = $row["CouponCode"];
-                $orderdate = $row["OrderDate"];
-                $ordername = $row["OrderName"];
-                $total = $row["Total"];
-
-            } else{
-                // URL doesn't contain valid id parameter. Redirect to error page
-                header("location: error.php");
-                exit();
-            }
-            
-        } else{
-            echo "Oops! Something went wrong. Please try again later.";
-        }
-    }
-     
-    // Close statement
-    mysqli_stmt_close($stmt);
-    
-    // Close connection
-    mysqli_close($link);
-} else{
-    // URL doesn't contain id parameter. Redirect to error page
-    header("location: error.php");
-    exit();
-}
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Maven Dashboard</title>
@@ -85,6 +29,7 @@ if(isset($_GET["ID"]) && !empty(trim($_GET["ID"]))){
         });
     </script>
 </head>
+
 <body>
     <div class="wrapper">
         <div class="container-fluid">
@@ -93,15 +38,20 @@ if(isset($_GET["ID"]) && !empty(trim($_GET["ID"]))){
                     <div class="mt-5 mb-3 clearfix">
                         <h2 class="pull-left">Order list for Maven's coupon code</h2>
                     </div>
-                    <?php
-                    // Include config file
+
+                <?php
+
+                   if(isset($_GET["ID"]) && !empty(trim($_GET["ID"]))){
+
+                    // Include config file to make the database connection
+
                     require_once "config.php";
-                    
-                    // Attempt select query execution
-                    
-                    $sql = "SELECT * from `Orders` where `CustID` = '51';";
+ 
+                    // Prepare a select statement
+                    $sql = "SELECT * FROM `Orders` WHERE `ID` = ?";
 
                     if($result = mysqli_query($link, $sql)){
+
                         if(mysqli_num_rows($result) > 0){
                             echo '<table class="table table-bordered table-striped">';
                                 echo "<thead>";
@@ -112,35 +62,47 @@ if(isset($_GET["ID"]) && !empty(trim($_GET["ID"]))){
                                         echo "<th>Order Date</th>";
                                         echo "<th>Order Total</th>";
 
-                                    echo "</tr>";
+                                echo "</tr>";
+                                
                                 echo "</thead>";
+
                                 echo "<tbody>";
+
                                 while($row = mysqli_fetch_array($result)){
                                     echo "<tr>";
                                         echo "<td>" . $row['CouponCode'] . "</td>";
                                         echo "<td>" . $row['OrderNumber'] . "</td>";
-                                        echo "<td>" . $row['OrderName'] . "</td>";
+                                        echo "<td>" . $row['OrderName'] . "</dtd>";
                                         echo "<td>" . $row['OrderDate'] . "</td>";
                                         echo "<td>" . $row['Total'] . "</td>";
                                     echo "</tr>";
                                 }
-                                echo "</tbody>";
+                            
+                            echo "</tbody>";
+
                             echo "</table>";
 
                             // Free result set
                             mysqli_free_result($result);
-                        } else{
+                        }
+
+                        elseif(mysqli_num_rows($result) = 0){
                             echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
                         }
-                    } else{
-                        echo "Oops! Something went wrong. Please try again later.";
-                    }
+                    
+                        else {
+                            echo "Oops! Something went wrong. Please try again later.";
+                        }
+                    
+                    // Close statement
+                    mysqli_stmt_close($stmt);
  
                     // Close connection
                     mysqli_close($link);
-                    ?>
+                    
+                ?>
 
-<a href="index.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i>Back to Dashboard</a>
+            <a href="index.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i>Back to Dashboard</a>
                 </div>
             </div>        
         </div>
